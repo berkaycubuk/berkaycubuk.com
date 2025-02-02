776aa2f42713090a676dacb43f41c7e39f00f49d
Berkay Çubuk <berkay@berkaycubuk.com>
2025-02-02 21:10:05 +0300

    feat: switched to sophist beta

---
+++ components/header.js
@@ -0,0 +1,10 @@
+export function Header() {
+  return (`<header class="header">
+      <div class="header-brand">Berkay Çubuk</div>
+      <nav class="header-nav">
+        <a href="/">Home</a>
+        <a href="/blog">Blog</a>
+        <a href="/about">About</a>
+      </nav>
+    </header>`);
+}
