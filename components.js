19f15c72077ac3c9b9cc790418c829e654144c85
Berkay Çubuk <berkay@berkaycubuk.com>
2025-02-04 21:14:49 +0300

    feat: new blog item added

--- components.js
+++ components.js
@@ -1,10 +1,14 @@
 export function Header() {
-  return (`<header class="header"><div class="container">
+  return (`<header class="header"><div class="container header-inner">
       <div class="header-brand">Berkay Çubuk</div>
       <nav class="header-nav">
         <a href="/">Home</a>
         <a href="/blog">Blog</a>
-        <a href="/about">About</a>
+        <a href="/about.html">About</a>
       </nav>
     </div></header>`);
+}
+
+export function Date(date) {
+  return (`<div class="content-date">${date}</div>`);
 }
