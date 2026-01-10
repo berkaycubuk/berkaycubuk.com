31adeef0982ef34886e9433dcdd5cb54791ce98a
Berkay Çubuk <berkay@berkaycubuk.com>
2026-01-10 22:29:17 +0300

    seo improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://berkaycubuk.com',
   vite: {
     plugins: [tailwindcss()]
   }
